67fb62effe30434195405b161d25ff870563aad7
dvhoang3 <dhoangvie@gmail.com>
2023-05-08 14:47:54 -0500

    set up server and expressjs for api calls

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const app = require('express')();
+const PORT = 8080;
+
+app.listen(
+  PORT,
+  () => console.log(`it's alive on http://localhost:${PORT}`)
+)
+
+app.get('/appetizers', (req, res) => {
+  res.status(200).send({
+    appetizers: []
+  })
+});
